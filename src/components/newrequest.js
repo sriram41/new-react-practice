476c5c2ed3aba5919b54f5d85392f1823c5a3695
sriram41 <133516424+sriram41@users.noreply.github.com>
2025-01-13 18:29:34 +0530

    Update newrequest.js

--- src/components/newrequest.js
+++ src/components/newrequest.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const newrequest = () => {
   return (
-    <div>newrequest some thing special</div>
+    <div>newrequest some thing special total contained</div>
   )
 }
 
